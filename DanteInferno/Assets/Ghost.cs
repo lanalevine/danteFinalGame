a060dc44378f1b1b52e8750c7ea5ce79dd1095c9
lanalevine <70363792+lanalevine@users.noreply.github.com>
2024-04-28 17:57:11 -0400

    fixed bug and began creating tilemap

--- DanteInferno/Assets/Ghost.cs
+++ DanteInferno/Assets/Ghost.cs
@@ -6,6 +6,12 @@ using UnityEngine.AI;
 public class Ghost : MonoBehaviour
 {
 
+    Animator animator;
+
+    private void Start(){
+        animator = GetComponent<Animator>();
+    }
+
     public float Health {
         set {
             health = value;
@@ -21,6 +27,22 @@ public class Ghost : MonoBehaviour
     public float health = 1;
 
     public void Defeated(){
+        animator.SetTrigger("Defeated");
+    }
+      private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Hitbox"){
+            
+            Debug.Log("this is a test 3");
+        
+            Health-=1;
+
+        }
+
+         Debug.Log("this is a test 4");
+
+    }
+
+    public void RemoveEnemy() {
         Destroy(gameObject);
     }
 }
